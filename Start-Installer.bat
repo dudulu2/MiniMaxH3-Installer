44b10c26ddbe8c927b2b38f68edbbb5db858eff7
dudulu2 <37184842+dudulu2@users.noreply.github.com>
2026-08-07 01:10:51 +0800

    Start installer with local wheel support

--- Start-Installer.bat
+++ Start-Installer.bat
@@ -1,5 +1,5 @@
 @echo off
 setlocal
 cd /d "%~dp0"
-powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -STA -File "%~dp0Install-MiniMaxH3.ps1"
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -STA -File "%~dp0Start-Installer.ps1"
 if errorlevel 1 pause
